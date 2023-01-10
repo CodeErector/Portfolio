58a68b46ec750cdbcc2c1d041d26acb3801b8c0d
CodeErector <ham78625@gmail.com>
2023-01-11 01:00:39 +0500

    leastFinal

---
+++ CodeErector/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿$(window).on("scroll", function () {
+    if ($(window).scrollTop() > 50) {
+        $(".header").addClass("hactive");
+        $(".scrolltop").addClass("scrolltopVisable");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+        $(".header").removeClass("hactive");
+        $(".scrolltop").removeClass("scrolltopVisable");
+    }
+})
