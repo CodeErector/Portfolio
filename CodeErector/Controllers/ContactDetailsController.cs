29fa70f4ad2310944dd520f54e087e75293a825a
CodeErector <ham78625@gmail.com>
2023-01-11 13:15:56 +0500

    Final

--- CodeErector/Controllers/ContactDetailsController.cs
+++ CodeErector/Controllers/ContactDetailsController.cs
@@ -18,6 +18,30 @@ namespace CodeErector.Controllers
             _context = context;
         }
 
+        // GET: ContactDetails
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.ContactDetails.ToListAsync());
+        }
+
+        // GET: ContactDetails/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.ContactDetails == null)
+            {
+                return NotFound();
+            }
+
+            var contactDetails = await _context.ContactDetails
+                .FirstOrDefaultAsync(m => m.ContactId == id);
+            if (contactDetails == null)
+            {
+                return NotFound();
+            }
+
+            return View(contactDetails);
+        }
+
         // GET: ContactDetails/Create
         public IActionResult Create()
         {
@@ -38,6 +62,99 @@ namespace CodeErector.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(contactDetails);
+        }
+
+        // GET: ContactDetails/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.ContactDetails == null)
+            {
+                return NotFound();
+            }
+
+            var contactDetails = await _context.ContactDetails.FindAsync(id);
+            if (contactDetails == null)
+            {
+                return NotFound();
+            }
+            return View(contactDetails);
+        }
+
+        // POST: ContactDetails/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ContactId,CDate,CustName,massage")] ContactDetails contactDetails)
+        {
+            if (id != contactDetails.ContactId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(contactDetails);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContactDetailsExists(contactDetails.ContactId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contactDetails);
+        }
+
+        // GET: ContactDetails/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.ContactDetails == null)
+            {
+                return NotFound();
+            }
+
+            var contactDetails = await _context.ContactDetails
+                .FirstOrDefaultAsync(m => m.ContactId == id);
+            if (contactDetails == null)
+            {
+                return NotFound();
+            }
+
+            return View(contactDetails);
+        }
+
+        // POST: ContactDetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.ContactDetails == null)
+            {
+                return Problem("Entity set 'projectDbContext.ContactDetails'  is null.");
+            }
+            var contactDetails = await _context.ContactDetails.FindAsync(id);
+            if (contactDetails != null)
+            {
+                _context.ContactDetails.Remove(contactDetails);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ContactDetailsExists(int id)
+        {
+          return _context.ContactDetails.Any(e => e.ContactId == id);
         }
     }
 }
